a1115ca681d70db5055b8dc530bed0b7e7d12e6c
Brianrahmarela <brian.rahmarela@gmail.com>
2021-02-04 16:56:48 +0700

    perbaikan soalno4.js

--- soalno4.js
+++ soalno4.js
@@ -1,32 +1,44 @@
-function f(n){
-  if(n == 0){
-    return 0
-  } else if (n >= 1 && n<=15) {
-    f(n-1) 
-    return 1
-  } else if (n >= 16 && n<=30) {
-    f(n-1) 
-    return 2
-  } else if (n >= 31 && n<=45) {
-    f(n-1) 
-    return 3
+function rangeAngka(n){
+  if (n <=0 ){
+    return 0;
+  } else if (n >=1 && n <=15) {
+    return 1;
+  } else {
+    return rangeAngka(n-15)+1;
   }
 }
 
-//bila param dari 0
-console.log(f(0))
+//TEST RANGE PARAM
 
-//bila param dari 1-15
-console.log(f(1))
-console.log(f(7))
-console.log(f(15))
+//<=0
+console.log(rangeAngka(0));
 
-//bila param dari 16-30
-console.log(f(16))
-console.log(f(20))
-console.log(f(30))
+//1-15
+console.log(rangeAngka(1));
+console.log(rangeAngka(2));
+console.log(rangeAngka(3));
+console.log(rangeAngka(15));
 
-//bila param dari 31-45
-console.log(f(31))
-console.log(f(40))
-console.log(f(45))
+//16-30
+console.log(rangeAngka(16));
+console.log(rangeAngka(17));
+console.log(rangeAngka(18));
+console.log(rangeAngka(30));
+
+//31-45
+console.log(rangeAngka(31));
+console.log(rangeAngka(45));
+
+//46-60
+console.log(rangeAngka(46));
+console.log(rangeAngka(60));
+
+//61-75
+console.log(rangeAngka(61));
+console.log(rangeAngka(75));
+
+//76-90
+console.log(rangeAngka(76));
+console.log(rangeAngka(90));
+
+
